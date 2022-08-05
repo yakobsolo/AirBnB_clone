3e1282688360e7188c09af0c47307ace7e4412f8
MubarekHussen <mubahussen2014@gmail.com>
2022-08-05 23:32:37 +0300

    cmd_6

--- console.py
+++ console.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+""" console.py - HBNBCommand"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    command line interpretor using cmd
+    """
+    prompt = "(hbnb) "
+
+    def do_EOF(self, line):
+        """end of a line
+        """
+        return True
+
+    def do_quit(self, line):
+        """Quit command to exit the program
+        """
+        raise SystemExit
+
+    def emptyline(self):
+        """
+        do nothing if empty line is entered
+        """
+        pass
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
