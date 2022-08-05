fd6b29da999edd21f2a46349dd22957986eb5ab9
yakobsolo <yakisolo2009@gmail.com>
2022-08-05 10:40:53 +0000

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,50 @@
 #!/usr/bin/python3
+""" file_storage.py - FileStorage """
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage():
+    """
+    private class attribute
+    and public methods
+    """
+    __file_path = "file.json"
+    __objects = {}
+    classes = {"BaseModel": BaseModel}
+
+    def all(self):
+        """
+        Return: __objects
+        """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """
+        creates new __objects
+        """
+        obj_key = obj.__class__.__name__ + "." + str(obj.id)
+        FileStorage.__objects[obj_key] = obj
+
+    def save(self):
+        """
+        serializes python object to json file
+        """
+        dict = {}
+        for i, j in FileStorage.__objects.items():
+            dict[i] = j.to_dict()
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(dict, f)
+
+    def reload(self):
+        """
+        deserializes json file to python object
+        """
+        try:
+            with open(FileStorage.__file_path) as f:
+                new_dict = json.load(f)
+            for i, j in new_dict.items():
+                obj = FileStorage.classes[j["__class__"]](**j)
+                FileStorage.__objects[i] = obj
+        except FileNotFoundError:
+            pass
