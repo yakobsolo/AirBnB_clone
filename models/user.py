940b06880a6c942d9cc5503c7be6afdba1d95b0e
yakobsolo <yakisolo2009@gmail.com>
2022-08-06 14:23:33 +0000

    update user

--- models/user.py
+++ models/user.py
@@ -6,6 +6,11 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """
     user class
+    attributes:
+    email
+    frist_name
+    last_name
+    password
     """
     email = ""
     first_name = ""
