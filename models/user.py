f4b43cbb1f98653cee1c144f5ad65e733fe906fa
yakobsolo <yakisolo2009@gmail.com>
2022-08-06 14:08:48 +0000

    first user

--- models/user.py
+++ models/user.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" user.py - User"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+    user class
+    """
+    email = ""
+    first_name = ""
+    last_name = ""
+    password = ""
