50cbe7cb635511634dc3c366cf9e4442bde4704d
yakobsolo <yakisolo2009@gmail.com>
2022-08-06 14:50:52 +0000

    updated

--- models/amenity.py
+++ models/amenity.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" amenity.py - Amenity """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+        class Amenity
+    """
+    name = ""
--- models/user.py
+++ models/user.py
@@ -6,11 +6,6 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """
     user class
-    attributes:
-    email
-    frist_name
-    last_name
-    password
     """
     email = ""
     first_name = ""
