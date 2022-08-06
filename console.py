6af785733e1b1d18002e60ccd9cbfbdf39083171
yakobsolo <yakisolo2009@gmail.com>
2022-08-06 14:19:39 +0000

    updated

--- console.py
+++ console.py
@@ -4,6 +4,7 @@ import cmd
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models import storage
+from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
